844e9a9b2787e745ab00b2df40a72abd70a107d3
Yevgeniya Kats <yevgeniyakats@Yevgeniyas-MBP.home>
2018-11-04 12:49:23 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   root 'exhibits#home'
 
-  get 'exhibits/home'
+  # get 'exhibits/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
